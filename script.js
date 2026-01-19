64c93c95327ae07e2f1cd00192e3d5712c8cf3c4
Bhuvanitha R <bhuvanitha2209@gmail.com>
2026-01-20 00:20:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const productArea = document.getElementById("productArea");
+
+function showMen() {
+  productArea.innerHTML = `
+    <img src="images/men1.jpg">
+    <img src="images/men2.jpg">
+    <img src="images/men3.jpg">
+  `;
+}
+
+function showWomen() {
+  productArea.innerHTML = `
+    <img src="images/women1.jpg">
+    <img src="images/women2.jpg">
+    <img src="images/women3.jpg">
+  `;
+}
+
+function showBabies() {
+  productArea.innerHTML = `
+    <img src="images/baby1.jpg">
+    <img src="images/baby2.jpg">
+    <img src="images/baby3.jpg">
+  `;
+}
+
+function showAccessories() {
+  productArea.innerHTML = `
+    <img src="images/acc1.jpg">
+    <img src="images/acc2.jpg">
+    <img src="images/acc3.jpg">
+  `;
+}
+
+
